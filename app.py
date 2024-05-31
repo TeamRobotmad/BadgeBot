acd9341809cfdd58877cd2e10b9b7edb1ff06770
Kenton Barnes <kentonbarnes@hotmail.co.uk>
2024-05-31 18:35:35 +0100

    no standard library

--- app.py
+++ app.py
@@ -1,10 +1,4 @@
-import asyncio
-from typing import Iterator, List, Optional, Tuple
-from itertools import chain
-
 import app
-import machine
-from enum import Enum
 
 from app_components.tokens import label_font_size
 from events.input import Buttons, BUTTON_TYPES
@@ -25,12 +19,12 @@ USER_TICK_MULTIPLIER = 4
 
 MAX_POWER = 100
 
-class BadgeBotAppState(Enum):
-    MENU = 1
-    RECEIVE_INSTR = 2
-    COUNTDOWN = 3
-    RUN = 4
-    DONE = 5
+# App states
+MENU = 1
+RECEIVE_INSTR = 2
+COUNTDOWN = 3
+RUN = 4
+DONE = 5
 
 class Instruction:
 
@@ -50,7 +44,7 @@ class Instruction:
     def __str__(self):
         return f"{self.press_type.name} {self._duration}"
 
-    def directional_power_tuple(self, power) -> Tuple[int, int, int, int]:
+    def directional_power_tuple(self, power):
         if self._press_type == BUTTON_TYPES["UP"]:
             return (power, 0, power, 0)
         elif self._press_type == BUTTON_TYPES["DOWN"]:
@@ -60,7 +54,7 @@ class Instruction:
         elif self._press_type == BUTTON_TYPES["RIGHT"]:
             return (0, power, power, 0)
 
-    def make_power_plan(self) -> Iterator[Tuple[int, int]]:
+    def make_power_plan(self):
         # return collection of tuples of power and their duration
         ramp_up = [(self.directional_power_tuple(p), TICK_MS)
                    for p in range(0, MAX_POWER, POWER_STEP_PER_TICK)]
@@ -92,7 +86,7 @@ class BadgeBotApp(app.App):
         self.power_plan_iter = iter([])
 
         # Overall app state
-        self.current_state = BadgeBotAppState.MENU
+        self.current_state = MENU
 
     def update(self, delta):
         if self.button_states.get(BUTTON_TYPES["CANCEL"]):
@@ -102,14 +96,14 @@ class BadgeBotApp(app.App):
             self.button_states.clear()
             self.minimise()
 
-        if self.current_state == BadgeBotAppState.MENU:
+        if self.current_state == MENU:
             # Exit start menu
             if self.button_states.get(BUTTON_TYPES["CONFIRM"]):
-                self.current_state = BadgeBotAppState.RECEIVE_INSTR
+                self.current_state = RECEIVE_INSTR
                 self.button_states.clear()
 
 
-        elif self.current_state == BadgeBotAppState.RECEIVE_INSTR:
+        elif self.current_state == RECEIVE_INSTR:
             # Enable/disable scrolling and check for long press
             if self.button_states.get(BUTTON_TYPES["CONFIRM"]):
 
@@ -119,7 +113,7 @@ class BadgeBotApp(app.App):
                 self.long_press_delta += delta
                 if self.long_press_delta >= LONG_PRESS_MS:
                     self.finalize_instruction()
-                    self.current_state = BadgeBotAppState.COUNTDOWN
+                    self.current_state = COUNTDOWN
 
             else:
                 # Confirm is not pressed. Reset long_press state
@@ -147,20 +141,20 @@ class BadgeBotApp(app.App):
                     self._handle_instruction_press(BUTTON_TYPES["DOWN"])
                     self.button_states.clear()
 
-        elif self.current_state == BadgeBotAppState.COUNTDOWN:
+        elif self.current_state == COUNTDOWN:
             self.run_countdown_ms += delta
             if self.run_countdown_ms >= self.run_countdown_target_ms:
-                self.power_plan_iter = chain(*(instr.power_plan_iterator for instr in self.instructions))
-                self.current_state = BadgeBotAppState.RUN
+                self.power_plan_iter = self.instructions[0].power_plan_iterator # TODO chain all instr
+                self.current_state = RUN
 
-        elif self.current_state == BadgeBotAppState.RUN:
+        elif self.current_state == RUN:
             print(delta)
             power = self.get_current_power_level(delta)
             if power is None:
-                self.current_state = BadgeBotAppState.DONE
+                self.current_state = DONE
             print(f"Using power: {power}")
 
-        elif self.current_state == BadgeBotAppState.DONE:
+        elif self.current_state == DONE:
             if self.button_states.get(BUTTON_TYPES["CONFIRM"]):
                 self.reset()
 
@@ -173,7 +167,7 @@ class BadgeBotApp(app.App):
         self.last_press = press_type
 
     def reset(self):
-        self.current_state = BadgeBotAppState.MENU
+        self.current_state = MENU
         self.button_states.clear()
         self.last_press: BUTTON_TYPES = BUTTON_TYPES["CANCEL"]
         self.long_press_delta = 0
@@ -200,29 +194,29 @@ class BadgeBotApp(app.App):
         else:
             ctx.rgb(0,0,0.1).rectangle(-120,-120,240,240).fill()
 
-        if self.current_state == BadgeBotAppState.MENU:
+        if self.current_state == MENU:
             ctx.rgb(1,1,1).move_to(H_START, V_START).text("To Program:")
             ctx.rgb(1,1,0).move_to(H_START, V_START + VERTICAL_OFFSET).text("Press C")
             ctx.rgb(1,1,1).move_to(H_START, V_START + 2*VERTICAL_OFFSET + 10).text("When finished:")
             ctx.rgb(1,1,0).move_to(H_START, V_START + 3*VERTICAL_OFFSET + 10).text("Long press C")
-        elif self.current_state == BadgeBotAppState.RECEIVE_INSTR:
+        elif self.current_state == RECEIVE_INSTR:
             for i_num, instr in enumerate(["START"] + self.instructions + [self.current_instruction, "END"]):
                 ctx.rgb(1,1,0).move_to(H_START, V_START + VERTICAL_OFFSET * (self.scroll_offset + i_num)).text(str(instr))
-        elif self.current_state == BadgeBotAppState.COUNTDOWN:
+        elif self.current_state == COUNTDOWN:
             ctx.rgb(1,1,1).move_to(H_START, V_START).text("Running in:")
             countdown_val = (self.run_countdown_target_ms - self.run_countdown_ms) / 1000
             ctx.rgb(1,1,0).move_to(H_START, V_START+VERTICAL_OFFSET).text(countdown_val)
-        elif self.current_state == BadgeBotAppState.RUN:
+        elif self.current_state == RUN:
             ctx.rgb(1,1,1).move_to(H_START, V_START).text("Running power")
-            ctx.rgb(1,0,0).move_to(H_START, V_START + VERTICAL_OFFSET).text(str(self.current_power_duration))
-        elif self.current_state == BadgeBotAppState.DONE:
+            ctx.rgb(1,0,0).move_to(H_START, V_START + 2*VERTICAL_OFFSET).text(str(self.current_power_duration))
+        elif self.current_state == DONE:
             ctx.rgb(1,1,1).move_to(H_START, V_START).text(f"Complete!")
             ctx.rgb(1,1,1).move_to(H_START, V_START + VERTICAL_OFFSET).text("To restart:")
             ctx.rgb(1,1,0).move_to(H_START, V_START + 2*VERTICAL_OFFSET).text("Press C")
         ctx.restore()
 
 
-    def get_current_power_level(self, delta) -> Optional[int]:
+    def get_current_power_level(self, delta) -> int:
         # takes in delta as ms since last call
         # if delta was > 20... what to do
         if delta >= TICK_MS:
