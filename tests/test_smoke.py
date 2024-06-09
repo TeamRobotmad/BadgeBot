67ea65f7a72966d162e8d55993520ded62cf55ed
Kenton Barnes <kentonbarnes@hotmail.co.uk>
2024-06-09 23:02:36 +0100

    fix hexdrive app init test

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -1,26 +1,32 @@
 import sys
+
 import pytest
 
 # Add badge software to pythonpath
 sys.path.append("../../../") 
 
 import sim.run
+from system.hexpansion.config import HexpansionConfig
 
 def test_import_badgebot_app_and_app_export():
-    from sim.apps.BadgeBot import BadgeBotApp
     import sim.apps.BadgeBot.app as BadgeBot
+    from sim.apps.BadgeBot import BadgeBotApp
     assert BadgeBot.__app_export__ == BadgeBotApp
 
 def test_import_hexdrive_app_and_app_export():
-    from sim.apps.BadgeBot.hexdrive import HexDriveApp
     import sim.apps.BadgeBot.hexdrive as HexDrive
+    from sim.apps.BadgeBot.hexdrive import HexDriveApp
     assert HexDrive.__app_export__ == HexDriveApp
 
 def test_badgebot_app_init():
     from sim.apps.BadgeBot import BadgeBotApp
     BadgeBotApp()
 
-@pytest.mark.xfail
-def test_hexdrive_app_init():
+def test_hexdrive_app_init(port):
     from sim.apps.BadgeBot.hexdrive import HexDriveApp
-    HexDriveApp()
+    config = HexpansionConfig(port)
+    HexDriveApp(config)
+
+@pytest.fixture
+def port():
+    return 1
