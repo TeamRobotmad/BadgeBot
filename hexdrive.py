e398ec142411833151b1370c5b88ba78c211d37f
robotmad <cbarnes@tmsl.com>
2024-06-10 22:41:25 +0100

    pwm frequency control, expose initialisation for re-init use. more robust use of LS pins in case other code corrupts them

--- hexdrive.py
+++ hexdrive.py
@@ -3,9 +3,7 @@
 # It is then run from the EEPROM by the BadgeOS.
 
 import asyncio
-
 from machine import I2C, PWM
-
 import app
 
 # HexDrive.py App Version - parsed by app.py to check if upgrade is required
@@ -14,91 +12,124 @@ APP_VERSION = 2
 POWER_ENABLE_PIN_INDEX = 0	# First LS pin used to enable the SMPSU
 POWER_DETECT_PIN_INDEX = 1  # Second LS pin used to sense if the SMPSU has a source of power
 
-PWM_FREQ = 20000    
+DEFAULT_PWM_FREQ = 20000    
 class HexDriveApp(app.App):
 
     def __init__(self, config=None):
         self.config = config
         self.power_state = None
+        self.pwm_setup_failed = True
+        self.last_update_time = 0
+        self.initialise()
+
+    def initialise(self) -> bool:
+        if self.config is None:
+            return False        
         # report app starting and which port it is running on
         print(f"HexDrive App Init on port {self.config.port}")
-        # Set Power Enable Pin to Output
-        self.power_control = self.config.ls_pin[POWER_ENABLE_PIN_INDEX]
-        self.power_detect = self.config.ls_pin[POWER_DETECT_PIN_INDEX]
-        self._set_pin_out(self.power_control.pin)  
+        # Set Power Detect Pin to Input and Power Enable Pin to Output
+        self.power_detect_pin = self.config.ls_pin[POWER_DETECT_PIN_INDEX]
+        self.power_control_pin = self.config.ls_pin[POWER_ENABLE_PIN_INDEX]
+        self._set_pin_direction(self.power_detect_pin.pin,  1)
+        self._set_pin_direction(self.power_control_pin.pin, 0)  
         self.set_power(False)
         # Set all HexDrive Hexpansion HS pins to low level outputs
         for hs_pin in self.config.pin:
             hs_pin.value(0)
     
         # Allocate PWM generation to pins
-        self.setup_failed = False
+        self.pwm_setup_failed = False
         self.PWMOutput = [None] * len(self.config.pin)
         for i_num, hs_pin in enumerate(self.config.pin):
             print(f"H:{self.config.port}:{i_num} PWM on pin {self.config.pin[i_num]}")
             try:
-                self.PWMOutput[i_num] = PWM(hs_pin, freq = PWM_FREQ, duty_u16 = 0)
+                self.PWMOutput[i_num] = PWM(hs_pin, freq = DEFAULT_PWM_FREQ, duty_u16 = 0)
                 print(self.PWMOutput[i_num])
             except:
                 # There are a finite number of PWM resources so it is possible that we run out
                 print(f"H:{self.config.port}:{i_num} PWM allocation failed")
-                self.setup_failed = True
-
+                self.pwm_setup_failed = True
+        return not self.setup_failed
 
     async def background_task(self):
         while 1:
-            # we will do something here probably
+            # 
             await asyncio.sleep(5)
 
 
     def get_status(self) -> bool:
+        return not self.setup_failed
+
+
+    # Turn the SPMPSU on or off
+    def set_power(self, state) -> bool:
+        if (self.config is None) or (state == self.power_state):
+            return False
+        print(f"HexDrive [{self.config.port}] Power={state}")
+        if (self._get_pin_value(self.power_detect.pin))
+            # if the power detect pin is high the the SMPSU has a power source so enable it
+            self._set_pin_state(self.power_control.pin, state)
+            self._set_pin_out(self.power_control.pin)  # in case it gets corrupted by other code
+        self.power_state = state
+        return self.power_state    
+
+
+    # Only one PWM frequency (in Hz) is supported for all outputs due to timer limitations
+    def set_freq(self, freq) -> bool:
         if self.setup_failed:
             return False
+        for i, pwm in enumerate(self.PWMOutput):
+            try:
+                pwm.freq(freq)
+                print(f"H:{self.config.port}:PWM[{i}] freq: {freq}Hz")
+            except:
+                print(f"H:{self.config.port}:PWM[{i}] freq: {freq}Hz set failed")
+                return False
         return True
+    
 
+    # Set all 4 PWM duty cycles in one go (0-65535)
+    def set_pwm(self, pwms) -> bool:
+        if self.pwm_setup_failed:
+            return False
+        for i, pwm in enumerate(pwms):
+            if pwm != self.PWMOutput[i].duty_u16():
+                # pwm duty cycle has changed so update it
+                print(f"H:{self.config.port}:PWM[{i}]:{pwm}")
+                self.PWMOutput[i].duty_u16(pwm)
+        return True
+    
 
-    def set_power(self, state):
-        if state == self.power_state:
-            return
-        print(f"HexDrive [{self.config.port}] Power={state}")
-        #TODO - check power_detect pin to see if power is present  
-        self._set_pin_value(self.power_control.pin, state)
-        self.power_state = state    
-
-
-    def _set_pin_value(self, pin, value):
+    def _set_pin_state(self, pin, state):
         try:
             i2c = I2C(7)
-            output_reg = int.from_bytes(i2c.readfrom_mem(pin[0], 0x02+pin[1], 1), 'little')
-            if value:
-                output_reg |= pin[2]
-            else:
-                output_reg &= ~(pin[2])
-            #print(f"H:Write to {hex(pin[0])} address {hex(0x02+pin[1])} value {hex(output_reg)}")
+            output_reg = i2c.readfrom_mem(pin[0], 0x02+pin[1], 1)[0]
+            output_reg = (output_reg | pin[2]) if state else (output_reg & ~pin[2])
+            print(f"H:Write to {hex(pin[0])} address {hex(0x02+pin[1])} value {hex(output_reg)}")
             i2c.writeto_mem(pin[0], 0x02+pin[1], bytes([output_reg]))
         except:
             print(f"H:access to I2C(7) blocked")
 
 
-    def _set_pin_out(self, pin):
+    def _get_pin_state(self, pin) -> bool:
+        try:
+            i2c = I2C(7)
+            input_reg = i2c.readfrom_mem(pin[0], 0x00+pin[1], 1)[0]
+            return (input_reg & pin[2]) != 0
+        except:
+            print(f"H:access to I2C(7) blocked")
+
+
+    def _set_pin_direction(self, pin, direction):
         try:
             # Use a Try in case access to i2C(7) is blocked for apps in future
             # presumably if this happens then the code will have been updated to
             # handle the GPIO direction correctly anyway.
             i2c = I2C(7)
-            config_reg = int.from_bytes(i2c.readfrom_mem(pin[0], 0x04+pin[1], 1), 'little')
-            config_reg &= ~(pin[2])
+            config_reg = i2c.readfrom_mem(pin[0], 0x04+pin[1], 1)[0]
+            config_reg = (config_reg | pin[2]) if (1 == direction) else (config_reg & ~pin[2])
             i2c.writeto_mem(pin[0], 0x04+pin[1], bytes([config_reg]))
         except:
-            print(f"access to I2C(7) blocked")
-
-
-    def set_pwm(self, pwms) -> bool:
-        if self.setup_failed:
-            return False
-        for i, pwm in enumerate(pwms):
-            print(f"Set PWM {i} to {pwm}")
-            self.PWMOutput[i].duty_u16(pwm)
-        return True
+            print(f"H:access to I2C(7) blocked")
     
 __app_export__ = HexDriveApp
