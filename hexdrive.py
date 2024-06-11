51062df996108f266a847fae4c687182681cf1da
robotmad <cbarnes@tmsl.com>
2024-06-11 21:10:12 +0100

    changed names to reduce .mpy file size
    (cherry picked from commit e83a9131b86272dcb98965912c0cccddc7938765)

--- hexdrive.py
+++ hexdrive.py
@@ -11,23 +11,23 @@ from system.scheduler.events import RequestStopAppEvent
 # HexDrive.py App Version - parsed by app.py to check if upgrade is required
 APP_VERSION = 2 
 
-POWER_ENABLE_PIN_INDEX = const(0)	# First LS pin used to enable the SMPSU
-POWER_DETECT_PIN_INDEX = const(1)  # Second LS pin used to sense if the SMPSU has a source of power
+_ENABLE_PIN = const(0)	# First LS pin used to enable the SMPSU
+_DETECT_PIN = const(1)   # Second LS pin used to sense if the SMPSU has a source of power
 
-DEFAULT_PWM_FREQ = const(20000)    
-DEFAULT_KEEP_ALIVE_PERIOD = const(1000)  # 1 second
+_DEFAULT_PWM_FREQ = const(20000)    
+_DEFAULT_KEEP_ALIVE_PERIOD = const(1000)  # 1 second
 class HexDriveApp(app.App):
 
     def __init__(self, config=None):
         self.config = config
-        self.keep_alive_period = DEFAULT_KEEP_ALIVE_PERIOD
+        self.keep_alive_period = _DEFAULT_KEEP_ALIVE_PERIOD
         self.power_state = None
         self.pwm_setup_failed = True
         self.last_update_time = 0
         self.outputs_energised = False
         self.PWMOutput = [None] * len(self.config.pin)
-        self.power_detect_pin = self.config.ls_pin[POWER_DETECT_PIN_INDEX]
-        self.power_control_pin = self.config.ls_pin[POWER_ENABLE_PIN_INDEX]       
+        self.power_detect = self.config.ls_pin[_DETECT_PIN]
+        self.power_control = self.config.ls_pin[_ENABLE_PIN]       
 
         eventbus.on_async(RequestStopAppEvent, self._handle_stop_app, self)
 
@@ -39,8 +39,8 @@ class HexDriveApp(app.App):
         # report app starting and which port it is running on
         print(f"HexDrive App Init on port {self.config.port}")
         # Set Power Detect Pin to Input and Power Enable Pin to Output
-        self._set_pin_direction(self.power_detect_pin.pin,  1)
-        self._set_pin_direction(self.power_control_pin.pin, 0)  
+        self._set_pin_direction(self.power_detect.pin,  1)
+        self._set_pin_direction(self.power_control.pin, 0)  
         self.set_power(False)
         # Set all HexDrive Hexpansion HS pins to low level outputs
         for hs_pin in self.config.pin:
@@ -48,7 +48,7 @@ class HexDriveApp(app.App):
         # Allocate PWM generation to pins
         for i_num, hs_pin in enumerate(self.config.pin):
             try:
-                self.PWMOutput[i_num] = PWM(hs_pin, freq = DEFAULT_PWM_FREQ, duty_u16 = 0)
+                self.PWMOutput[i_num] = PWM(hs_pin, freq = _DEFAULT_PWM_FREQ, duty_u16 = 0)
                 print(f"H:{self.config.port}:PWM[{i_num}]:{self.PWMOutput[i_num]}")
             except:
                 # There are a finite number of PWM resources so it is possible that we run out
@@ -64,6 +64,8 @@ class HexDriveApp(app.App):
             pwm.deinit()
             self.PWMOutput[i] = None
         self.set_power(False)
+        for hs_pin in self.config.pin:
+            hs_pin.value(0)          
         return True
 
     async def _handle_stop_app(self, event):
@@ -96,11 +98,11 @@ class HexDriveApp(app.App):
     def set_power(self, state) -> bool:
         if (self.config is None) or (state == self.power_state):
             return False
-        print(f"H:[{self.config.port}]:Power={'On' if state else 'Off'}")
+        print(f"H:{self.config.port}:Power={'On' if state else 'Off'}")
         if (self._get_pin_state(self.power_detect_pin.pin)):
             # if the power detect pin is high then the SMPSU has a power source so enable it
-            self._set_pin_state(self.power_control_pin.pin, state)
-            self._set_pin_direction(self.power_control_pin.pin, 0)  # in case it gets corrupted by other code
+            self._set_pin_state(self.power_control.pin, state)
+            self._set_pin_direction(self.power_control.pin, 0)  # in case it gets corrupted by other code
         self.power_state = state
         return self.power_state    
 
@@ -150,7 +152,7 @@ class HexDriveApp(app.App):
             i2c.writeto_mem(pin[0], 0x02+pin[1], bytes([output_reg]))
             print(f"H:Write to {hex(pin[0])} address {hex(0x02+pin[1])} value {hex(output_reg)}")
         except Exception as e:
-            print(f"H:access to I2C(7) failed: {e}")
+            print(f"H:{self.config.port}:access to I2C(7) failed: {e}")
 
 
     def _get_pin_state(self, pin) -> bool:
@@ -159,7 +161,7 @@ class HexDriveApp(app.App):
             input_reg = i2c.readfrom_mem(pin[0], 0x00+pin[1], 1)[0]
             return (input_reg & pin[2]) != 0
         except Exception as e:
-            print(f"H:access to I2C(7) failed: {e}")
+            print(f"H:{self.config.port}:access to I2C(7) failed: {e}")
 
 
     def _set_pin_direction(self, pin, direction):
@@ -172,6 +174,6 @@ class HexDriveApp(app.App):
             config_reg = (config_reg | pin[2]) if (1 == direction) else (config_reg & ~pin[2])
             i2c.writeto_mem(pin[0], 0x04+pin[1], bytes([config_reg]))
         except Exception as e:
-            print(f"H:access to I2C(7) failed: {e}")
+            print(f"H:{self.config.port}:access to I2C(7) failed: {e}")
     
 __app_export__ = HexDriveApp
