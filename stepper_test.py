b49c2072ff08049859edfb1a8e517a0f088f6278
robotmad <cbarnes@tmsl.com>
2026-03-30 00:51:46 +0100

    use @property & @setter to make code neater for steppers.

--- stepper_test.py
+++ stepper_test.py
@@ -100,17 +100,35 @@ class Stepper:
     
 
     @property
-    def max_pos(self):
+    def max_pos(self) -> int:
         return self._max_pos
 
-    def step_size(self, sz=1):
+    @max_pos.setter
+    def max_pos(self, mp: int):
+        """ Set the maximum position for the stepper (in full steps)."""
+        self._max_pos = 2 * int(mp)
+
+
+    @property
+    def step_size(self) -> int:
+        return self._step_size
+    
+    @step_size.setter
+    def step_size(self, sz: int):
+        """ Set the step size (microstepping level) for the stepper.  Valid values are 1 (full step) or 2 (half step)."""
         if sz < 1:
             sz = 1
         elif sz > 2:
             sz = 2
         self._step_size = int(sz)
 
-    def speed(self, sps):
+
+    @property
+    def speed(self) -> int:
+        return self._steps_per_sec
+
+    @speed.setter
+    def speed(self, sps: int):
         if self._free_run_mode == 1 and sps < 0:
             self._free_run_mode = -1
         elif self._free_run_mode == -1 and sps > 0:
@@ -122,13 +140,22 @@ class Stepper:
         self._steps_per_sec = int(sps)
         self._update_timer((2 // self._step_size) * abs(self._steps_per_sec))
 
-    def speed_rps(self, rps):
-        self.speed(rps * self._steps_per_rev)
 
-    def get_speed(self):
-        return self._steps_per_sec
+    @property
+    def speed_rps(self) -> float:
+        return self._steps_per_sec / self._steps_per_rev
+    
+    @speed_rps.setter
+    def speed_rps(self, rps: float):
+        self.speed = int(rps * self._steps_per_rev)
 
-    def target(self, t):
+
+    @property
+    def target(self) -> int:
+        return self._target_pos // 2
+
+    @target.setter
+    def target(self, t: int):
         if self._calibrated and t < 0:
             self._target_pos = 0
         elif self._calibrated and (2 * int(t)) > self._max_pos:
@@ -136,31 +163,44 @@ class Stepper:
         else:
             self._target_pos = 2 * int(t)
 
-    def target_deg(self, deg):
-        self.target(self._steps_per_rev * deg / 360.0)
 
-    def target_rad(self, rad):
-        self.target(self._steps_per_rev * rad / (2 * pi))
+    @property
+    def target_deg(self) -> float:
+        return self._target_pos * 180.0 / self._steps_per_rev
 
-    def get_pos(self):
+    @target_deg.setter
+    def target_deg(self, deg: float):
+        self.target = int(self._steps_per_rev * deg / 360.0)
+
+
+    @property
+    def target_rad(self) -> float:
+        return self._target_pos * pi / self._steps_per_rev
+
+    @target_rad.setter
+    def target_rad(self, rad: float):
+        self.target = int(self._steps_per_rev * rad / (2 * pi))
+
+
+    @property
+    def pos(self) -> int:
+        """ Get the current position of the stepper in full steps.  Note that the internal position is tracked in half-steps to allow for microstepping, so the returned value is the internal position divided by 2."""
         return (self._pos // 2)
 
-    def get_pos_deg(self):
-        return self._pos * 180.0 / self._steps_per_rev
-
-    def get_pos_rad(self):
-        return self._pos * pi / self._steps_per_rev
-
-    def overwrite_pos(self, p=0):
+    @pos.setter
+    def pos(self, p: int):
         self._pos = 2 * int(p)
 
-    def overwrite_pos_deg(self, deg):
-        self._pos = deg * self._steps_per_rev / 180.0
+    @property
+    def pos_deg(self) -> float:
+        return self._pos * 180.0 / self._steps_per_rev
 
-    def overwrite_pos_rad(self, rad):
-        self._pos = rad * self._steps_per_rev / pi
+    @property
+    def pos_rad(self) -> float:
+        return self._pos * pi / self._steps_per_rev
 
-    def step(self, d=0):
+
+    def step(self, d: int = 0):
         cur_time = time.ticks_ms()
         if time.ticks_diff(cur_time, self._last_step_time) < self._min_period:
             return
@@ -174,12 +214,12 @@ class Stepper:
         if self._calibrated and self._pos < 0:
             print("s/w min endstop")
             self._pos = 0
-            self.speed(0)
+            self.speed = 0
             return
         elif self._calibrated and self._pos > self._max_pos:
             print("s/w max endstop")
             self._pos = self._max_pos
-            self.speed(0)
+            self.speed = 0
             return
         try:
             self._hexdrive_app.motor_step(self._phase)
@@ -191,11 +231,11 @@ class Stepper:
         print("Endstop - hit")
         if not self._calibrated:
             self._calibrated = True
-        self.overwrite_pos(0)
+        self.pos = 0
         if self._free_run_mode < 0:
-            self.speed(0)
+            self.speed = 0
         elif self._free_run_mode == 0 and self._target_pos < self._pos:
-            self.speed(0)
+            self.speed = 0
 
 
     def _timer_callback_fwd(self, t):   # pylint: disable=unused-argument
@@ -276,8 +316,8 @@ class Stepper:
                 self._update_timer(0)
                 self._hexdrive_app.motor_release()
             self._hexdrive_app.set_power(e)
-        except Exception:      # pylint: disable=broad-except
-            print("enable failed")
+        except Exception as ex:      # pylint: disable=broad-except
+            print(f"enable failed {ex}")
 
 
     def is_enabled(self):
@@ -369,57 +409,59 @@ class StepperTestMgr:
         if app.button_states.get(BUTTON_TYPES["RIGHT"]):
             if app.auto_repeat_check(delta, True):
                 if self.stepper_mode == StepperMode.SPEED:
-                    speed = self.stepper.get_speed()
+                    speed = self.stepper.speed
                     speed = inc_value(speed, app.auto_repeat_level + 1)
                     if _STEPPER_MAX_SPEED < speed:
                         speed = _STEPPER_MAX_SPEED
-                    self.stepper.speed(speed)
+                    self.stepper.speed = speed
                 else:
                     if self.stepper_mode != StepperMode.POSITION:
                         self.stepper_mode.mode = StepperMode.POSITION
-                        self.stepper.speed(_STEPPER_DEFAULT_SPEED)
+                        self.stepper.speed = _STEPPER_DEFAULT_SPEED
                         self.stepper.track_target()
-                    pos = self.stepper.get_pos()
+                    pos = self.stepper.pos
                     pos = inc_value(pos, app.auto_repeat_level + 1)
-                    self.stepper.target(pos)
+                    self.stepper.target = pos
                 app.refresh = True
         elif app.button_states.get(BUTTON_TYPES["LEFT"]):
             if app.auto_repeat_check(delta, True):
                 if self.stepper_mode == StepperMode.SPEED:
-                    speed = self.stepper.get_speed()
+                    speed = self.stepper.speed
                     speed = dec_value(speed, app.auto_repeat_level + 1)
                     if -_STEPPER_MAX_SPEED > speed:
                         speed = -_STEPPER_MAX_SPEED
-                    self.stepper.speed(speed)
+                    self.stepper.speed = speed
                 else:
                     if self.stepper_mode != StepperMode.POSITION:
                         self.stepper_mode.mode = StepperMode.POSITION
-                        self.stepper.speed(_STEPPER_DEFAULT_SPEED)
+                        self.stepper.speed = _STEPPER_DEFAULT_SPEED
                         self.stepper.track_target()
-                    pos = self.stepper.get_pos()
+                    pos = self.stepper.pos
                     pos = dec_value(pos, app.auto_repeat_level + 1)
-                    self.stepper.target(pos)
+                    self.stepper.target = pos
                 app.refresh = True
         else:
             app.auto_repeat_clear()
             if app.button_states.get(BUTTON_TYPES["CANCEL"]):
                 app.button_states.clear()
-                if app.hexdrive_app is not None:
-                    self.stepper.enable(False)
+                self.stepper.enable(False)
+                
                 app.return_to_menu()
                 return True
-            elif app.button_states.get(BUTTON_TYPES["CONFIRM"]):
+            elif app.button_states.get(BUTTON_TYPES["CONFIRM"]):    #"Mode" button
                 app.button_states.clear()
                 self.stepper_mode.inc()
                 if self.stepper_mode == StepperMode.POSITION:
-                    self.stepper.speed(_STEPPER_DEFAULT_SPEED)
-                    self.stepper.target(self.stepper.get_pos())
+                    self.stepper.speed = _STEPPER_DEFAULT_SPEED
+                    self.stepper.target = self.stepper.pos
                     self.stepper.track_target()
                 elif self.stepper_mode == StepperMode.SPEED:
-                    self.stepper.speed(0)
+                    self.stepper.speed = 0
                     self.stepper.free_run(1)
                 else:
+                    # "Off" mode - stop the stepper and disable coils to save power
                     self.stepper.stop()
+                    self.stepper.speed = 0
                 app.refresh = True
                 app.notification = Notification(f"  Stepper:\n {self.stepper_mode}")
                 print(f"Stepper:{self.stepper_mode}")
@@ -429,7 +471,7 @@ class StepperTestMgr:
             self.time_since_last_input += delta
             if self.time_since_last_input > self.timeout_period:
                 self.stepper.stop()
-                self.stepper.speed(0)
+                self.stepper.speed = 0
                 self.stepper.enable(False)
                 app.return_to_menu()
                 app.notification = Notification("  Stepper:\n Timeout")
@@ -470,7 +512,7 @@ class StepperTestMgr:
             background_colour = (0.15, 0.15, 0.15)
             ctx.rgb(*background_colour).rectangle(-100, 1, 200, label_font_size - 2).fill()
             c = 0
-            x = 200 * (self.stepper.get_pos() / self.stepper.max_pos) - 100
+            x = 200 * (self.stepper.pos / self.stepper.max_pos) - 100
             ctx.rgb(*bar_colour).rectangle(x - 2, 1, 5, label_font_size - 2).fill()
             ctx.rgb(*body_colour)
             if x > (c + 4):
@@ -480,9 +522,9 @@ class StepperTestMgr:
             ctx.rgb(0, 0, 0).move_to(c, 0).line_to(c, label_font_size).stroke()
             ctx.restore()
             if self.stepper_mode == StepperMode.SPEED:
-                stepper_text[i + 1] = f"{int(self.stepper.get_speed()):4}/s"
+                stepper_text[i + 1] = f"{int(self.stepper.speed):4}/s"
             else:
-                stepper_text[i + 1] = "Off" if (self.stepper_mode == StepperMode.OFF) else f"{int(self.stepper.get_pos()):+6} "
+                stepper_text[i + 1] = "Off" if (self.stepper_mode == StepperMode.OFF) else f"{int(self.stepper.pos):+6} "
         app.draw_message(ctx, stepper_text, stepper_text_colours, label_font_size)
         button_labels(ctx, confirm_label="Mode", cancel_label="Back", left_label="<--", right_label="-->")
         return True
